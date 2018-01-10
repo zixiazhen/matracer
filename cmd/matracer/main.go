93e03bb59d268f13e243135fad3c3cf4172faf88
zixzhen <zixzhen@cisco.com>
2018-01-10 14:25:01 -0800

    minor update

--- cmd/matracer/main.go
+++ cmd/matracer/main.go
@@ -52,20 +52,24 @@ func main() {
 	//Create and delete stream
 	flag.BoolVar(&enableStreamCreation, "enablestream", false, "Enable create and delete stream.")
 	flag.IntVar(&numOfStream, "numofstream", 2, "The number of stream that will be created.")
-	flag.IntVar(&streamCreateDeleteFrequency, "streamperiod", 30, "The period that we create and delete stream.")
+	flag.IntVar(&streamCreateDeleteFrequency, "streamperiod", 60, "The period that we create and delete stream.")
 
 	//Scale RC
 	flag.BoolVar(&enableRCScaling, "enablescale", false, "This will scale rc in and out.")
 
 	flag.Parse()
 
+	//This is a stop signal that from os, when we kill the application from os, the app should do clean up and then stop.
 	var gracefulStop = make(chan os.Signal)
 	signal.Notify(gracefulStop, syscall.SIGTERM)
 	signal.Notify(gracefulStop, syscall.SIGINT)
 	signal.Notify(gracefulStop, syscall.SIGSYS)
 	signal.Notify(gracefulStop, syscall.SIGKILL)
 
+	//This is used for app internal stop signal.
 	stop := make(chan error)
+	//This channel is used by MA tracer to notify other components to stop.
+	//For example, when we observe that two MA hold the same stream, we should notify streamCreater to stop creating stream.
 	errChal := make(chan error)
 
 	if enableTrace == true {
@@ -97,7 +101,7 @@ func main() {
 }
 
 
-func TraceMA(errChl chan error, stop chan os.Signal){
+func TraceMA(errChl chan error, gracefulStop chan os.Signal){
 
 	//end point full path
 	maEndpointFullPath := apiserver + endpointuri + MA_ENDPOINT_NAME
@@ -109,7 +113,7 @@ func TraceMA(errChl chan error, stop chan os.Signal){
 		select {
 		case <-ticker.C:
 			goTraceMA(maEndpointFullPath, errChl)
-		case <-stop:
+		case <-gracefulStop:
 			ticker.Stop()
 			fmt.Printf("%v", "Stopping MA Tracer! \n")
 			return
@@ -117,7 +121,7 @@ func TraceMA(errChl chan error, stop chan os.Signal){
 	}
 }
 
-func CreateAndDeleteStream(errChl chan error, stop chan os.Signal) {
+func CreateAndDeleteStream(errChl chan error, gracefulStop chan os.Signal) {
 
 	//end point full path
 	nsaEndpointFullPath := apiserver + endpointuri + NSA_ENDPOINT_NAME
@@ -134,7 +138,7 @@ func CreateAndDeleteStream(errChl chan error, stop chan os.Signal) {
 
 			//collectLogs()
 			break
-		case <- stop:
+		case <- gracefulStop:
 			break
 		}
 	}
@@ -161,7 +165,7 @@ func goCreateAndDeleteStream(nsaEndpointFullPath string){
 			fmt.Printf("Create stream failed: %v \n", err.Error())
 		}
 		//Sleep for a while, wait for the creation done.
-		time.Sleep(20 * time.Second)
+		time.Sleep(30 * time.Second)
 
 		//3. Delete all streams
 		//stream.DeleteAll(nsaRestCall)
@@ -170,7 +174,7 @@ func goCreateAndDeleteStream(nsaEndpointFullPath string){
 			fmt.Printf("Delete stream failed: %v \n", err.Error())
 		}
 		//Sleep for a while, wait for the deletion done.
-		time.Sleep(10 * time.Second)
+		time.Sleep(20 * time.Second)
 	}
 }
 
@@ -329,116 +333,6 @@ func printResult(result map[string]string) {
 
 }
 
-
-
 func scaleRC(errChl chan error, stop chan os.Signal) {
 	fmt.Printf("Coming Soon! \n")
 }
-
-
-/*
-func runOld(endpointFullPath string, stop chan error) {
-
-	//This map cache the endpoint addr for the MA pods.
-	endpointsMap := make(map[string]*api.ObjectReference) //IP : Ref
-
-	//This map is use as a set. to determine if there are duplicates
-	streamMap := make(map[string]string) //StreamID : IP
-
-	//This map is used for show the result
-	// MA that own a stream:	PodName : "StreamID"
-	// MA that is idle:			PodName : "idle"
-	result := make(map[string]string)
-
-	// Do a simple get from k8s api server
-	resp, err := rest.R().Get(endpointFullPath)
-	if err != nil {
-		fmt.Printf("Cannot access server! \n")
-		return
-	}
-
-	// get a list of endpoints
-	//fmt.Printf("%s",resp.Body())
-	var eps api.Endpoints
-	err = json.Unmarshal(resp.Body(), &eps)
-	if err != nil {
-		fmt.Print("Unmarshal resp body failed! \n")
-		return
-	}
-
-	//var addresses []string
-	//need to verify if in all case, there is only one subset in endpoints
-	if eps.Subsets == nil || len(eps.Subsets) == 0 || len(eps.Subsets[0].Addresses) == 0 {
-		fmt.Printf("No endpoint information found! Will quit this program!\n")
-		return
-	}
-	endpointAddrList := eps.Subsets[0].Addresses
-	port := eps.Subsets[0].Ports[0].Port
-
-	//Get all IPs from endpoints, add to endpointsMap
-	for _, endpointAddr := range endpointAddrList {
-		endpointsMap[endpointAddr.IP] = endpointAddr.TargetRef
-	}
-
-	//range endpointsMap
-	for epIPAddr, epObjRef := range endpointsMap {
-		if epObjRef == nil {
-			fmt.Print("Object Reference is nil, skip this pod! \n")
-			continue
-		}
-
-		//1. Create the MA status rest call url
-		maStatusRestCall := fmt.Sprintf("http://%s:%v/status", epIPAddr, port)
-		//fmt.Printf("maStatusRestCall: %s \n",maStatusRestCall)
-
-		//2. Get steam ID from MA Status
-		maStatusRaw, err := rest.R().Get(maStatusRestCall)
-		if err != nil {
-			fmt.Printf("Cannot access Manifest Agent: %s \n", epIPAddr)
-			result[epObjRef.Name] = "Down"
-			continue
-		}
-
-		var maStatus api.MAStatus
-		err = json.Unmarshal(maStatusRaw.Body(), &maStatus)
-		if err != nil {
-			fmt.Print("Unmarshal ma status resp body failed!")
-			result[epObjRef.Name] = "Status Unknown"
-			continue
-		}
-
-		//3. Check if there are multiple MA hold the same Stream ID
-		if len(maStatus.StreamID) != 0 {
-			//this pod own a stream.
-			//Check if there is another endpoint already own the stream.
-			if anotherEndpoint, found := streamMap[maStatus.StreamID]; found {
-				fmt.Printf(" =======================================================  \n")
-				fmt.Printf(" ============ Multi-MA own the same steam!! ============  \n")
-				fmt.Printf(" | Strean ID: 	%v  \n", maStatus.StreamID)
-				fmt.Printf(" | MA-1: 		%v  \n", epObjRef.Name)
-				fmt.Printf(" | MA-2: 		%v  \n", endpointsMap[anotherEndpoint].Name)
-				fmt.Printf(" =======================================================  \n")
-
-				//add a record result
-				result[epObjRef.Name] = maStatus.StreamID + "****"
-				result[endpointsMap[anotherEndpoint].Name] = maStatus.StreamID + "****"
-
-				//create a error file in /tmp folder
-				createFile()
-			} else {
-				streamMap[maStatus.StreamID] = epIPAddr
-				//add a record result
-				result[epObjRef.Name] = maStatus.StreamID
-			}
-		} else {
-			//add a record result
-			result[epObjRef.Name] = "Idle"
-		}
-	}
-
-	//Print result
-	printResult(result)
-}
-
-*/
-
